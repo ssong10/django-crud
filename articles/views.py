3a7e5cc2722029fde1fdaa0650639925814880fb
Ssong10 <leesy1403@naver.com>
2019-08-19 15:42:54 +0900

    03 | articles index

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+def index(request):
+    articles = Article.objects.all()
+
+    context = {
+        'articles' : articles
+    }
+    for article in articles:
+        context[articles] = article
+    return render(request, 'articles/index.html',context)

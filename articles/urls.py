3ad134f587de0b68e81c3df78ba02ebf1c777595
Ssong10 <leesy1403@naver.com>
2019-08-19 16:41:00 +0900

    04 | articles | CRUD - C

--- articles/urls.py
+++ articles/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
+    path('new/', views.new),
+    path('create/', views.create),
+
 ]
+
--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+import requests
+
 # Create your views here.
 def index(request):
-    articles = Article.objects.all()
+    articles = Article.objects.order_by('-id')
 
     context = {
         'articles' : articles
     }
-    for article in articles:
-        context[articles] = article
     return render(request, 'articles/index.html',context)
+
+def new(request):
+    return render(request,'articles/new.html')
+
+def create(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    # article = Article.objects.create(title=title, content=content)
+    article = Article(title=title, content=content)
+    article.save()
+
+    context = {
+        'article': article
+    }
+    # return render(request,'articles/create.html',context)
+    return redirect('/articles/')

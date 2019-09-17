0a761f61cf4e2acf2b74e5eb3cad6491733d465a
Ssong10 <leesy1403@naver.com>
2019-09-17 11:45:26 +0900

    17 | Update django form to modelForm

--- articles/views.py
+++ articles/views.py
@@ -25,10 +25,10 @@ def create(request):
         # embed()
         if article_form.is_valid():
         # 검증에 성공하면 저장하고,
-            title = article_form.cleaned_data.get('title')
-            content = article_form.cleaned_data.get('content')
-            article = Article(title=title, content=content)
-            article.save()
+            # title = article_form.cleaned_data.get('title')
+            # content = article_form.cleaned_data.get('content')
+            # article = Article(title=title, content=content)
+            article = article_form.save()
             # redirect
             return redirect('articles:detail', article.pk)
         # else :
@@ -73,21 +73,13 @@ def delete(request,article_pk):
 def update(request,article_pk):
     article = Article.objects.get(pk=article_pk)
     if request.method =='POST':
-        article_form = ArticleForm(request.POST)
+        article_form = ArticleForm(request.POST, instance=article)
         if article_form.is_valid():
-            article.title = article_form.cleaned_data.get('title')
-            article.content = article_form.cleaned_data.get('content')
-            article.save()
+            article = article_form.save()
             return redirect('articles:detail',article_pk)
     else:
-        article_form = ArticleForm(
-            initial={
-                'title':article.title,
-                'content':article.content
-                }
-        )
+        article_form = ArticleForm(instance=article)
     context = {
-        'article':article,
         'article_form' : article_form
     }
     return render(request,'articles/form.html',context)
